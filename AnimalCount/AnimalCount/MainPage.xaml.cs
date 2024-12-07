105470733170029d40fd5efdde6fa67801c21d7a
Austin Donnelly <austind@microsoft.com>
2024-12-07 15:37:44 +0000

    initial commit

---
+++ AnimalCount/AnimalCount/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AnimalCount
+{
+    public class ScoringAnimal
+    {
+        public ScoringAnimal(string name, int score)
+        {
+            this.Score = score;
+            this.Name = name;
+        }
+
+        public int Score { get; }
+        public string Name { get; }
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        int score = 0;
+
+        List<ScoringAnimal> scoringAnimals = new List<ScoringAnimal>()
+        {
+            new ScoringAnimal("Sheep", 1),
+            new ScoringAnimal("Cow", 3),
+            new ScoringAnimal("Dog", 1),
+            new ScoringAnimal("Pig", 4),
+            new ScoringAnimal("Bird", 1),
+            new ScoringAnimal("Cat", 2),
+            new ScoringAnimal("Horse", 5),
+            new ScoringAnimal("Other", 10)
+        };
+
+        public List<ScoringAnimal> ScoringAnimals { get { return this.scoringAnimals; } }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.collectionView.BindingContext = this;
+        }
+
+        private void ResetScore_Clicked(object sender, EventArgs e)
+        {
+            score = 0;
+            updateScoreDisplay();
+        }
+
+        private void updateScoreDisplay()
+        {
+            this.scoreLabel.Text = String.Format("{0}", this.score);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var animalName = button.Text;
+            var animal = (ScoringAnimal)button.BindingContext;
+            //var animal = findAnimalByName(animalName);
+            score += animal.Score;
+            updateScoreDisplay();
+        }
+
+        private ScoringAnimal findAnimalByName(string name)
+        {
+            foreach (var animal in ScoringAnimals)
+            {
+                if (animal.Name == name)
+                    return animal;
+            }
+
+            throw new ApplicationException($"animal={name} not found");
+        }
+    }
+}
---
+++ AnimalCountNative/MainActivity.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Snackbar;
+
+
+namespace AnimalCount
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        int score = 0;
+        Android.Widget.TextView scoreTextView;
+
+        (int, int)[] idScores = new (int, int)[]
+        {
+            (Resource.Id.sheep, 1),
+            (Resource.Id.cow, 3),
+            (Resource.Id.dog, 1),
+            (Resource.Id.pig, 4)
+        };
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            this.scoreTextView = FindViewById<Android.Widget.TextView>(Resource.Id.scoreTextView);
+            this.scoreTextView.Text = String.Format("score: {0}", score);
+
+            var ids = new int[] { Resource.Id.sheep, Resource.Id.cow, Resource.Id.dog, Resource.Id.pig };
+            foreach (var (id,score) in idScores)
+            {
+                var button = FindViewById<Android.Widget.Button>(id);
+                button.Click += AddOneButton_Click;
+            }
+        }
+
+        private void AddOneButton_Click(object sender, EventArgs e)
+        {
+            var button = (Android.Widget.Button)sender;
+            int the_score = -1;
+            foreach (var (id,score) in idScores)
+            {
+                if (id == button.Id)
+                {
+                    the_score = score;
+                    break;
+                }
+            }
+
+            score += the_score;
+            this.scoreTextView.Text = String.Format("score: {0}", score);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+   
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+	}
+}
